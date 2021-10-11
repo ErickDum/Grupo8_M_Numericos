86c87989393aa8bf6b01145a48895ce1456c5340
ErickDum <erick.dumas@ucuenca.edu.ec>
2021-10-11 11:33:21 -0500

    :+1:

--- Newton_Rapson/newton.m
+++ Newton_Rapson/newton.m
@@ -1,4 +1,14 @@
-function [datos]=newton(f, df, x0, n_i, tol = 0.001)
+% NEWTON RAPSON  Raices de ecuaciones (didáctica).
+% NEWTON(F, DF, X0, N_1, TOL)
+%    [DATOS] = NEWTON(F, DF, X0, N_1, TOL)
+%     datos = matriz de resultados 
+%     f = funcion a tratar
+%     df = derivada de la funcion a tratar
+%     x0 = punto inicial del cual parten las interaciones
+%     n_i = numero de iteraciones
+%     tol = tolerancia
+
+function [datos]=newton(f, df, x0, n_i = 100, tol = 0.001)
    datos = zeros(3,1);
    if (df(x0)==0)
      error('No es posible hallar la raiz')
