18bce35976e09f5458762a2e6f77961413d12ffc
ErickDum <erick.dumas@ucuenca.edu.ec>
2021-10-25 12:06:40 -0500

    :+1:

--- Sistema de ecuaciones lineales/Gauss/main.m
+++ Sistema de ecuaciones lineales/Gauss/main.m
@@ -18,11 +18,16 @@ clear, clc
 
 fprintf('Tensiones en nodos \n');
 
+figure(1)
+imagen1 = imread('circuito1.png');
+imshow(imagen1);
+
 try
-a = [2, 0, -1; 0, 2, -1; -2, -1, 7];
-b = [-8; 8; 0];
+a = [3, -1; -3, 5];
+b = [20;60];
 
 [x] = gauss(a,b)
+
 catch err
 fprintf('Error: %s\n',err.message)
 end
@@ -35,9 +40,13 @@ end
 
 fprintf('Tensiones en nodos \n');
 
+figure(2)
+imagen2 = imread('circuito2.png');
+imshow(imagen2);
+
 try
-a = [3, -2, 0; 2, -7, 5; 0, 2, -3];
-b = [4; 4; -16];
+a = [3, -2, -1; -4, 7, -1; 2, -3, 1];
+b = [12; 0; 0];
 
 [x] = gauss(a,b)
 catch err
