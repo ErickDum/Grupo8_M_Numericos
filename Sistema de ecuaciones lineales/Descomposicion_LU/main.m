9c4b945c948c1fc614dacc3b10a6e5f8b51e199d
ErickDum <erick.dumas@ucuenca.edu.ec>
2021-10-27 23:11:39 -0500

    :+1:

--- Sistema de ecuaciones lineales/Descomposicion_LU/main.m
+++ Sistema de ecuaciones lineales/Descomposicion_LU/main.m
@@ -16,17 +16,42 @@ clear, clc
 
 fprintf('Tensiones en nodos \n');
 
+figure(1)
 imagen1 = imread('circuito1.png');
 imshow(imagen1)
 
 try
-a = [3, -2, -1; -4, 7, -1; 2, -3, 1];
+a = [4, 0, -2; 0, 2, -1; -2, -1, 7];
+b = [-16; 8; 0];
+
+[L, U]=lu(a);
+
+y = sustitucion_progresiva(L, b);
+V = sustitucion_regresiva(U, y)
+
+catch err
+fprintf('Error: %s\n',err.message)
+end
+
+
+% PROBLEMA 2
+% Un circuito se resuelve mediante analisis de mallas, a partir de ahi se 
+% plantean una matriz de G*V=I
+
+fprintf('Tensiones en nodos \n');
+
+figure(2)
+imagen1 = imread('circuito2.png');
+imshow(imagen1)
+
+try
+a = [11, -5, -6; -5, 19, -2; -1, -1, 2];
 b = [12; 0; 0];
 
 [L, U]=lu(a);
 
 y = sustitucion_progresiva(L, b);
-x = sustitucion_regresiva(U, y)
+I = sustitucion_regresiva(U, y)
 
 catch err
 fprintf('Error: %s\n',err.message)
