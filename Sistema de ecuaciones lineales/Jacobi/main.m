a876e1f03d83b260d524f87df9f37e126e998c29
ErickDum <erick.dumas@ucuenca.edu.ec>
2021-11-02 12:36:31 -0500

    :+1:

--- Sistema de ecuaciones lineales/Jacobi/main.m
+++ Sistema de ecuaciones lineales/Jacobi/main.m
@@ -11,14 +11,39 @@
 clear, clc
 
 %PROBLEMA 1
+% Un circuito se resuelve mediante analisis de mallas, a partir de ahi se 
+% plantean una matriz de R*I=V
+
+fprintf('Corrientes en mallas \n');
+
+figure(1)
+imagen1 = imread('circuito1.png');
+imshow(imagen1)
 
 try
-A = [4, 0, -2; 0, 2, -1; -2, -1, 7];
-b = [-16; 8; 0];
+A = [-2000, 2000; -6000, 8000];
+b = [0; 3];
 
-[x, Error, Iteraciones] = jacobi(A, b)
+[I, Error, Iteraciones] = jacobi(A, b)
 catch err
 fprintf('Error: %s\n',err.message)
 end
 
 %PROBLEMA 2
+% Un circuito se resuelve mediante analisis de mallas, a partir de ahi se 
+% plantean una matriz de R*I=V
+
+fprintf('Corrientes en mallas \n');
+
+figure(2)
+imagen2 = imread('circuito2.png');
+imshow(imagen2)
+
+try
+A = [-1, 2, 0; 0, 1, 0; -2000, 0, 8000];
+b = [0; 0.002; 3];
+
+[I, Error, Iteraciones] = jacobi(A, b)
+catch err
+fprintf('Error: %s\n',err.message)
+end
