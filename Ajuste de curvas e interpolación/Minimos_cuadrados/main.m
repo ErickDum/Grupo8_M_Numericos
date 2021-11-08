36f44e373faf507b769d1f16bb27d56318df5b14
ErickDum <erick.dumas@ucuenca.edu.ec>
2021-11-08 02:41:12 -0500

    :+1:

---
+++ Ajuste de curvas e interpolación/Minimos_cuadrados/main.m
@@ -0,0 +1,88 @@
+% Autores: Mateo Chimbo
+%          Erick Dumas
+%          Karla Esterilla
+%
+% Fecha: 08/11/2021
+%
+% Descripcion: 
+% Implementacion del metodo ajuste por minimos cuadrados para
+% una regresion lineal.
+
+clear, clc
+
+% Problema 1:
+% La directora de salubridad de Chapel Hill esta interesada en saber
+% la relacion que existe entre la antiguedad  de un camion de basura
+% y los gastos anuales de reparacion. Con este fin la directora a 
+% reunido la informacion de cuatro camiones:
+
+disp('PROBLEMA 1')
+
+try
+x = [5, 3 ,3, 1]; %Antiguedad (años)
+y = [7; 7; 6; 4]; %Costos de reparacion
+
+[Sr, a0, a1] = minimos_cuadrados(x, y);
+
+fprintf('Suma de los cuadrados de los residuos: \n')
+fprintf('%.4f', Sr)
+fprintf('\n')
+
+y_modelo = @(x) a0 + a1*x;
+
+figure(1)
+fplot(y_modelo,[0, 10])
+legend ("y_modelo", 'Interpreter', 'latex');
+title('Relacion entre antiguedad y costos de reparacion', 'Interpreter', 'latex')
+xlabel('X', 'Interpreter', 'latex')
+ylabel('Y', 'Interpreter', 'latex')
+grid on
+hold on
+
+for i=1:numel(x)
+  plot(x(i), y(i), 'o')
+endfor
+
+catch err
+fprintf('Error: %s\n',err.message);
+end
+
+hold off;
+
+% Problema 2:
+% Se requiere un pronostico de demanda haciendo uso de la información 
+% histórica de venta de un producto determinado durante los últimos 
+% 12 trimestres (3 años).
+
+disp('PROBLEMA 1')
+
+try
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]; % Meses
+y = [600; 1550; 1500; 1500; 2400; 3100; 2600; 2900; 3800; 4500; 4000; 4900]; %Ventas
+
+[Sr, a0, a1] = minimos_cuadrados(x, y);
+
+fprintf('Suma de los cuadrados de los residuos: \n')
+fprintf('%.4f', Sr)
+fprintf('\n')
+
+y_modelo = @(x) a0 + a1*x;
+
+figure(2)
+fplot(y_modelo,[0, 15])
+legend ("y_modelo", 'Interpreter', 'latex');
+title('Pronóstico de Demanda', 'Interpreter', 'latex')
+xlabel('X', 'Interpreter', 'latex')
+ylabel('Y', 'Interpreter', 'latex')
+grid on
+hold on
+
+for i=1:numel(x)
+  plot(x(i), y(i), 'o')
+endfor
+
+catch err
+fprintf('Error: %s\n',err.message);
+end
+
+hold off;
---
+++ Ajuste de curvas e interpolación/Minimos_cuadrados/minimos_cuadrados.m
@@ -0,0 +1,23 @@
+% MINIMOS CUADRADOS regresion lineal (didáctica).
+% MINIMOS_CUADRADOS(X, Y)
+%       [Sr, a0, a1]=minimos_cuadrados(x, y)
+%        Sr = suma de los cuadrados de los residuos
+%        a0 = parametro para econtrar la aproximacion lineal
+%        a1 = parametro para econtrar la aproximacion lineal
+%        x = vector de filas con los valores de x
+%        y = vector de columnas con los valores de y
+
+function [Sr, a0, a1]=minimos_cuadrados(x, y)
+  if (numel(x)!=numel(y))
+    error('Matrices de tamaños incorrectos')
+  endif
+  n = numel(x);
+  ym = sum(y)/n;
+  xm = sum(x)/n;
+  a1 = (n*(x*y)-sum(x)*sum(y))/(n*sum(x.^2)-sum(x).^2);
+  a0 = ym - a1*xm;
+  Sr = 0;
+  for i=1:n
+    Sr = (y(i)-a0-a1*x(i)).^2;
+  endfor
+end
