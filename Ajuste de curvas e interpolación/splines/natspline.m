c6114d295bb594444f1a2267c554279749b1d5c5
ErickDum <erick.dumas@ucuenca.edu.ec>
2021-11-18 15:32:19 -0500

    :+1:

--- Ajuste de curvas e interpolación/splines/natspline.m
+++ Ajuste de curvas e interpolación/splines/natspline.m
@@ -20,23 +20,24 @@ function [yy,dy,d2] = natspline(x,y,xx)
   b = zeros(n,n);
   aa(1,1) = 1; aa(n,n) = 1;
   bb(1) = 0; bb(n) = 0;
-  for i = 2:n-1
+  for i = 2:n-1 %Diferencias finitas
     aa(i,i-1) = h(x, i - 1);
     aa(i,i) = 2 * (h(x, i - 1) + h(x, i));
     aa(i,i+1) = h(x, i);
     bb(i) = 3 * (fd(i + 1, i, x, y) - fd(i, i - 1, x, y));
   endfor
-  c = aa\bb'; 
-  for i = 1:n - 1 
+  c = aa\bb';  %Resuelve c coeficientes
+  for i = 1:n - 1 %Resuelve coeficientes a, b y d
     a(i) = y(i);
     b(i) = fd(i + 1, i, x, y) - h(x, i) / 3 * (2 * c(i) + c(i + 1));
     d(i) = (c(i + 1) - c(i)) / 3 / h(x, i);
   endfor
-  for i = 1:m 
+  for i = 1:m %Realiza interpolaciones en los valores deseados
     [yy(i),dy(i),d2(i)] = SplineInterp(x, n, a, b, c, d, xx(i));
   endfor
 end
 
+%Diferencias Finitas
 function hh = h(x, i)
   hh = x(i + 1) - x(i);
 end
@@ -45,6 +46,7 @@ function fdd = fd(i, j, x, y)
   fdd = (y(i) - y(j)) / (x(i) - x(j));
 end
 
+%Determina los valores y derivadas en los valores deseados
 function [yyy,dyy,d2y] = SplineInterp(x, n, a, b, c, d, xi)
   for ii = 1:n - 1
     if xi >= x(ii) - 0.000001 & xi <= x(ii + 1) + 0.000001
