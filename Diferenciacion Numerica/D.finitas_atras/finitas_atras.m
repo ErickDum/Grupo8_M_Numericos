fb6cd9bcf607e752734e67efa86c793075883f12
ErickDum <erick.dumas@ucuenca.edu.ec>
2021-11-19 01:48:44 -0500

    :+1:

---
+++ Diferenciacion Numerica/D.finitas_atras/finitas_atras.m
@@ -0,0 +1,17 @@
+function [d1, d2] = finitas_atras(f, x)
+  n = numel(x);
+  if (n==1)
+    error('El vector con valores independientes debe contener mas de un valor')
+  endif
+  d1 = zeros(2, n-2);
+  d2 = zeros(2, n-3);
+  h = (x(n)-x(1)+1)/n;
+  for i=3:n
+    d1(1, i-2) = x(i);
+    d1(2, i-2) = (3*f(x(i)) - 4*f(x(i-1)) + f(x(i-2)))/2*h;
+  endfor
+  for i=4:n
+    d2(1,i-3) = x(i);
+    d2(2,i-3) = (2*f(x(i)) - 5*f(x(i-1)) + 4*f(x(i-2)) - f(x(i-3)))/h^2;
+  endfor
+end
---
+++ Diferenciacion Numerica/D.finitas_atras/main.m
@@ -0,0 +1,65 @@
+% Autores: Mateo Chimbo
+%          Erick Dumas
+%          Karla Esterilla
+%
+% Fecha: 11/10/2021
+%
+% Descripcion: 
+% Implementacion del metodo de Newton-Raphson para hallar las raices 
+% en ecuaciones no lineales.
+
+clear, clc, clf;
+
+% PROBLEMA 1
+% En un circuito RLC la corriente esta dada por i = 10sen(3t). Hallar 
+% el voltaje en el inductor si L = 15.
+
+disp('PROBLEMA 1')
+
+try
+  
+i = @(t) 10*sin(3*t);
+t = linspace(0,10,100);
+
+[d1, d2] = finitas_atras(i, t);
+
+Vl = 15*d1(2,:)
+
+figure(1)
+
+fplot(i, [0, 10])
+hold on
+grid on
+plot(d1(1,:), Vl)
+legend('Corriente', 'Voltaje en inductor')
+
+catch err
+fprintf('Error: %s\n',err.message);
+end
+
+% PROBLEMA 2
+% En un circuito RLC la corriente esta dada por i = 30sen(5t). Hallar 
+% el voltaje en el inductor si L = 15.
+
+disp('PROBLEMA 2')
+
+try
+  
+i = @(t) 30*sin(5*t);
+t = linspace(0,10,100);
+
+[d1, d2] = finitas_atras(i, t);
+
+Vl = 15*d1(2,:)
+
+figure(2)
+
+fplot(i, [0, 10])
+hold on
+grid on
+plot(d1(1,:), Vl)
+legend('Corriente', 'Voltaje en inductor')
+
+catch err
+fprintf('Error: %s\n',err.message);
+end
