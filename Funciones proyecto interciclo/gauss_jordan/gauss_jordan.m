a6cb9dc70cc1ae1f700a37a6492ed74e72983f77
KarlaEsterilla <karla.esterilla@ucuenca.edu.ec>
2021-11-22 01:08:36 -0500

    .
    .

---
+++ Funciones proyecto interciclo/gauss_jordan/gauss_jordan.m
@@ -0,0 +1,37 @@
+% Autores: Mateo Chimbo
+%          Erick Dumas          
+%          Karla Esterilla
+% Fecha:   28 de octubre de 2021
+% Descripcion: Esta funcion implemeta el algoritmo de gauss_jordan para resolver sistemas de ecuaciones 
+%              con varias incognitas
+%gauss_jordan    Solucion de sistemas de ecuaciones.
+%   gauss_jordan(A) llenar
+%       A=Matriz
+%       resultados=Solucion de la matriz
+%       
+
+
+
+function[resultados]=gauss_jordan(A)
+
+for i=1:rows(A)
+  pivote=A(i,i);
+  if (pivote==0)
+    error('No tiene solucion unica')
+  endif
+  A(i,:)=A(i,:)/pivote; 
+  A([1 i],:)=A([i 1],:); 
+  
+  for l=2:rows(A)
+    l;
+    A(l,:)=A(l,:)+(A(1,:)*-A(l,i));
+    
+  endfor
+  
+  A([i 1],:)=A([1 i],:);
+ 
+endfor
+resultados=A(:,columns(A));
+
+########No poner msgbox en la funcion 
+#####3333poner el err para que capte el try catch
